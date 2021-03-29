22d9317f0f6762b9789e3942a777d0a6de47635f
Md. Ala Uddin <pial469048@gmail.com>
2021-03-29 13:35:26 +0600

    Detail of Selected blog with its author, name and datetime shown

--- js/myjs.js
+++ js/myjs.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       blogs: [],
+      selectedBlog: null
     };
   },
   methods: {
@@ -10,9 +11,15 @@ const app = Vue.createApp({
         .then((response) => response.json())
         .then((json) => (this.blogs = json.blogs));
     },
+    selectBlog(id) {
+        fetch("http://vue-test.gingerbd.com/api/view-blog/" + id)
+          .then((response) => response.json())
+          .then((json) => (this.selectedBlog = json.blogs[0]));
+    }
   },
   mounted() {
       this.loadBlogs();
+      this.selectBlog(1);
   }
 });
 
