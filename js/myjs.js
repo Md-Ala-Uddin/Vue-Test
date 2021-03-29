ab1f6cd137c6712dd93dee2f050491f794a7f285
Md. Ala Uddin <pial469048@gmail.com>
2021-03-29 13:53:54 +0600

    Commenter name, comment received with dattime and function created to submit them but unfortunately theres a CORS missing allow origin error , unknown to me preventing them from being submitted

--- js/myjs.js
+++ js/myjs.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       blogs: [],
-      selectedBlog: null
+      selectedBlog: null,
+      newComment: { user: "", comment: "" },
     };
   },
   methods: {
@@ -12,15 +13,33 @@ const app = Vue.createApp({
         .then((json) => (this.blogs = json.blogs));
     },
     selectBlog(id) {
-        fetch("http://vue-test.gingerbd.com/api/view-blog/" + id)
-          .then((response) => response.json())
-          .then((json) => (this.selectedBlog = json.blogs[0]));
-    }
+      fetch("http://vue-test.gingerbd.com/api/view-blog/" + id)
+        .then((response) => response.json())
+        .then((json) => (this.selectedBlog = json.blogs[0]));
+    },
+    postComment() {
+      fetch("http://vue-test.gingerbd.com/public/api/post-comment", {
+        method: "POST",
+        body: JSON.stringify({
+          blog_id: this.selectedBlog.id,
+          comment: this.newComment.comment,
+          comment_datetime: new Date(),
+          user: this.newComment.user,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+        .then(() => $("#NewCommentModal").modal("hide"))
+        .catch(error => console.log(error));
+    },
   },
   mounted() {
-      this.loadBlogs();
-      this.selectBlog(1);
-  }
+    this.loadBlogs();
+    this.selectBlog(1);
+  },
 });
 
 const mountedApp = app.mount('#page-top');
